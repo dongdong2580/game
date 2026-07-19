08d66c6a85dfaa44035008fe8fdadec581087545
dongdong2580 <dongdong5481@gmail.com>
2026-07-19 21:20:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+// RIPOSTE 서비스 워커: 오프라인 실행 및 PWA 설치 가능성을 위한 최소 캐싱
+const CACHE_NAME = 'riposte-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // 구글 폰트 등 외부 리소스는 네트워크 우선, 그 외 정적 자산은 캐시 우선
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
